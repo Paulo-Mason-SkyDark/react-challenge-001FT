f2dd118931f8ecb0b220ef5ff8ea03b0001bc3e9
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-08-28 18:47:29 -0400

    wip modify

--- hapu/src/components/header/index.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-export default About => <h1>About us</h1>
--- hapu/src/pages/index.js
+++ hapu/src/pages/index.js
@@ -3,7 +3,6 @@ import About from '../components/header/index';
 export default function Home() {
   return (
     <>
-    <About/>
       <h1>Hapu</h1>
   </>   
   );
