d5bec9a81e1e5c4edc8e7c372e1135140c93eae6
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-08-28 18:02:20 -0400

    wip- modify files

--- hapu/src/pages/_document.js
+++ hapu/src/pages/_document.js
@@ -33,9 +33,8 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet"/>
-          <title>Hapu</title>
+        <title>Hapu</title>
         </Head>
         <body>
           <Main />
